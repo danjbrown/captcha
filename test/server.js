16af8677c8a44da0f5f2e93d3e51b7e739a57a7b
Dan Brown <dbrown.email@gmail.com>
2018-02-24 12:10:10 +0000

    New: MIT license

--- test/server.js
+++ test/server.js
@@ -6,33 +6,32 @@ let should      = chai.should();
 
 chai.use(chaiHttp);
 
-describe('2 Factor Authentication Service', () => {
-    // Test create secret key
-    describe('GET /create-secret-key', () => {
-        it('it should create a secret key', (done) => {
+describe('Captcha Service', () => {
+    // Test create Captcha
+    describe('GET /create', () => {
+        it('it should create a Captcha image', (done) => {
             chai.request(server)
-            .get('/create-secret-key')
+            .get('/create')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.have.property('success');
-                res.body.should.have.property('secretKey');
-                res.body.should.have.property('qrCodeData');
+                res.body.should.have.property('data');
                 done();
             });
         });
     });
 
-    // Test the verify TOTP route
-    describe('POST /verify-totp', () => {
-        it('it should verify a TOTP', (done) => {
+    // Test the verify Captcha code route
+    describe('POST /verify', () => {
+        it('it should verify a Captcha code', (done) => {
             chai.request(server)
-            .post('/verify-totp')
+            .post('/verify')
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({userId: 123456, totp: 224466})
+            .send({code: 'HG6adF'})
             .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property('success', false);
-                res.body.should.have.property('message', 'Invalid TOTP');
+                res.body.should.have.property('message', 'Invalid code');
                 done();
             });
         });
